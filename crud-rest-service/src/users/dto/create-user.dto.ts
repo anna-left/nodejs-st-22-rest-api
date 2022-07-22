82c4544cf144c2e536201d514e6b0770f729fcf4
anna-left <anna_left@mail.ru>
2022-07-22 15:21:21 +0300

    feat: edit password validation

--- crud-rest-service/src/users/dto/create-user.dto.ts
+++ crud-rest-service/src/users/dto/create-user.dto.ts
@@ -1,6 +1,6 @@
 import Joi from 'joi';
 
-import { IsString, IsNumber, IsBoolean, Matches } from 'class-validator';
+import { IsString, IsNumber, IsBoolean } from 'class-validator';
 
 export class CreateUserDto {
   @IsString()
@@ -10,10 +10,6 @@ export class CreateUserDto {
   readonly email: string;
 
   @IsString()
-  @Matches(/^(?=.*[0-9])(?=.*[a-zA-Z])([a-zA-Z0-9]+)$/, {
-    message:
-      'Password must contain at least 1 number and 1 character in a string',
-  })
   readonly password: string;
 
   @IsNumber()
@@ -23,10 +19,18 @@ export class CreateUserDto {
   readonly isDeleted: boolean;
 }
 
+const passwordPattern = /^(?=.*[0-9])(?=.*[a-zA-Z])([a-zA-Z0-9]+)$/;
+
 export const UserSchema = Joi.object({
   login: Joi.string().required(),
   email: Joi.string().email().required(),
-  password: Joi.string().alphanum().required(),
+  password: Joi.string()
+    .required()
+    .pattern(new RegExp(passwordPattern))
+    .messages({
+      'string.pattern.base':
+        'Password must consist of letters and numbers (and contain at least 1 number and 1 character in a string)',
+    }),
   age: Joi.number().integer().min(4).max(130).required(),
   isDeleted: Joi.boolean().required(),
 }).options({
