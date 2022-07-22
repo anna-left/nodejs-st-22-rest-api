fac6b09b582c0ba267f04e1a31f48a8aaac70496
anna-left <anna_left@mail.ru>
2022-07-22 12:01:01 +0300

    feat: generate users

---
+++ crud-rest-service/src/users/users.controller.ts
@@ -0,0 +1,83 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  Query,
+  Res,
+  HttpStatus,
+} from '@nestjs/common';
+import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { PaginationQueryDto } from 'src/common/dto/pagination-query.dto';
+import { LoginSubstringUserDto } from './dto/getLoginSubstring-user.dto';
+
+@Controller('v1/users')
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Post()
+  create(@Body() createUserDto: CreateUserDto) {
+    return this.usersService.create(createUserDto);
+  }
+
+  @Get('/limitusers')
+  getAutoSuggestUsers(
+    @Body() loginSubstringUserDto: LoginSubstringUserDto,
+    @Res() response,
+  ) {
+    const users = this.usersService.getAutoSuggestUsers(loginSubstringUserDto);
+    if (!users) {
+      return response.status(HttpStatus.NOT_FOUND).send('Not Found');
+    }
+    return response.status(HttpStatus.OK).send(users);
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string, @Res() response) {
+    const user = this.usersService.findOne(id);
+    if (!user) {
+      return response.status(HttpStatus.NOT_FOUND).send('Not Found');
+    }
+    return response.status(HttpStatus.OK).send(user);
+  }
+
+  @Get()
+  findAll(@Query() paginationQuery: PaginationQueryDto) {
+    const { limit, offset } = paginationQuery;
+    return this.usersService.findAll(limit, offset);
+  }
+
+  @Patch(':id')
+  update(
+    @Param('id') id: string,
+    @Body() updateUserDto: UpdateUserDto,
+    @Res() response,
+  ) {
+    const user = this.usersService.findOne(id);
+    if (!user) {
+      return response.status(HttpStatus.NOT_FOUND).send('Not Found');
+    }
+    const updUser = this.usersService.update(id, updateUserDto);
+    return response.status(HttpStatus.OK).send(updUser);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string, @Res() response) {
+    const user = this.usersService.findOne(id);
+    if (!user) {
+      return response.status(HttpStatus.NOT_FOUND).send('Not Found');
+    }
+    this.usersService.remove(id);
+    if (user.isDeleted) {
+      return response
+        .status(HttpStatus.OK)
+        .send('The object was successfully deleted');
+    }
+    return response.status(HttpStatus.NOT_FOUND).send('Not Found');
+  }
+}
