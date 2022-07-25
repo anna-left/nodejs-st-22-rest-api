4ce49bf4c910fade0ed37a348eb21826e7e996cf
anna-left <anna_left@mail.ru>
2022-07-25 15:04:41 +0300

    feat: install dependencies

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import 'dotenv/config';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    UsersModule,
+    SequelizeModule.forRoot({
+      dialect: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      port: Number(process.env.POSTGRES_PORT),
+      username: process.env.POSTGRES_USERNAME,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DATABASE,
+      models: [],
+      autoLoadModels: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
