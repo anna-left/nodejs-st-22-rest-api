145c31cb1e5e59c769024ea8d55d36c7974dd26e
anna-left <anna_left@mail.ru>
2022-08-02 14:07:10 +0300

    regactor: delete unused fragments

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,7 +14,6 @@ import { User } from './users/users.model';
     SequelizeModule.forRoot({
       dialect: 'postgres',
       host: process.env.POSTGRES_HOST,
-      port: Number(process.env.POSTGRES_PORT),
       username: process.env.POSTGRES_USERNAME,
       password: process.env.POSTGRES_PASSWORD,
       database: process.env.POSTGRES_DATABASE,
