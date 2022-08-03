1ed7b55d2da7ad4df625dde4c913172b328e6a56
anna-left <anna_left@mail.ru>
2022-08-03 21:44:59 +0300

    refactor: handle the response in the controller

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,77 +1,68 @@
-import {
-  BadRequestException,
-  Injectable,
-  NotFoundException,
-} from '@nestjs/common';
-import { ApiOperation, ApiResponse } from '@nestjs/swagger';
+import { Injectable } from '@nestjs/common';
 
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { SearchUserDto } from './dto/search-user.dto';
-import { User } from './users.model';
 import { UsersRepository } from './users.repository';
+import { HTTP_RESPONS_MESSAGES } from './utils/constants';
 
 @Injectable()
 export class UsersService {
   constructor(private usersRepository: UsersRepository) {}
 
-  @ApiOperation({ summary: 'User creation' })
-  @ApiResponse({ status: 200, type: User })
   async createUser(createUserDto: CreateUserDto) {
     const user = await this.usersRepository.findByLogin(createUserDto.login);
     if (user) {
-      throw new BadRequestException(
-        `User ${createUserDto.login} already exists in the database`,
-      );
+      return {
+        message: HTTP_RESPONS_MESSAGES.USER_EXISTS,
+      };
     }
-    return await this.usersRepository.create(createUserDto);
+    return { value: await this.usersRepository.create(createUserDto) };
   }
 
-  @ApiOperation({ summary: 'Get all users' })
-  @ApiResponse({ status: 200, type: [User] })
   async findAll(params: SearchUserDto) {
     const substring =
       params.loginSubstring === undefined ? '' : params.loginSubstring;
     return await this.usersRepository.findAll(substring, Number(params.limit));
   }
 
-  @ApiOperation({ summary: 'Get one user by id' })
-  @ApiResponse({ status: 200, type: [User] })
-  async findOne(id: string): Promise<User> {
+  async findOne(id: string) {
     const user = await this.usersRepository.findOne(id);
     if (!user) {
-      throw new NotFoundException("User does'n exist");
+      return {
+        message: HTTP_RESPONS_MESSAGES.USER_NOT_FOUND,
+      };
     }
-    return user;
+    return { value: user };
   }
 
-  @ApiOperation({ summary: 'Update user' })
-  @ApiResponse({ status: 200, type: User })
   async update(id: string, updateUserDto: UpdateUserDto) {
     const user = await this.usersRepository.findOne(id);
     if (!user) {
-      throw new NotFoundException("User does'n exist");
+      return {
+        message: HTTP_RESPONS_MESSAGES.USER_NOT_FOUND,
+      };
     }
     if (
       updateUserDto.hasOwnProperty('login') &&
       updateUserDto['login'] !== user.login
     ) {
       if (await this.usersRepository.findByLogin(updateUserDto['login'])) {
-        throw new BadRequestException(
-          `User ${updateUserDto['login']} already exists in the database`,
-        );
+        return {
+          message: HTTP_RESPONS_MESSAGES.USER_EXISTS,
+        };
       }
     }
-    return await user.update(updateUserDto);
+    return { value: await user.update(updateUserDto) };
   }
 
-  @ApiOperation({ summary: 'Update user' })
-  @ApiResponse({ status: 200, type: User })
   async remove(id: string) {
     const user = await this.usersRepository.findOne(id);
     if (!user) {
-      throw new NotFoundException("User does'n exist");
+      return {
+        message: HTTP_RESPONS_MESSAGES.USER_NOT_FOUND,
+      };
     }
-    return await user.update({ isDeleted: true });
+    return { value: await user.update({ isDeleted: true }) };
   }
 }
