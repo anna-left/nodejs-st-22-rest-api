aff677a57fb5334656b1e602b358c651fb940dfa
anna-left <anna_left@mail.ru>
2022-08-10 08:20:06 +0300

    refactor: remove unused value

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,5 +1,4 @@
 const HTTP_RESPONS_MESSAGES = {
-  INVALID_UUID_FORMAT: 'Invalid uuid format',
   USER_NOT_FOUND: "User does'n exist",
   USER_EXISTS: 'User with this login already exists in the database',
   USER_EXISTS_DELETED:
