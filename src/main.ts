b82371ccdf1eb48d91bef8550f0914a1714355ab
anna-left <anna_left@mail.ru>
2022-08-18 17:10:57 +0300

    feat: Add error handling to process.on

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './modules/app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import 'dotenv/config';
-import { myLogger } from './middlewares/logger';
+import { myLogger, winstonLogger } from './middlewares/logger';
 import { HandleRespInterceptor } from './interceptors/handle-resp.interceptor';
 
 async function bootstrap() {
@@ -12,6 +12,25 @@ async function bootstrap() {
     bufferLogs: true,
   });
   app.use(myLogger);
+
+  process.on('uncaughtException', (error, origin) => {
+    winstonLogger.log({
+      level: 'error',
+      message: `uncaughtException: ${error.message}, origin: ${origin}`,
+      error,
+    });
+    process.exit();
+  });
+  //throw new Error('my test error - uncaughtException');
+
+  process.on('unhandledRejection', (reason, promise) => {
+    winstonLogger.log({
+      level: 'error',
+      message: `UnhandledRejection: ${promise}, reason: ${reason}`,
+    });
+    process.exit();
+  });
+  //Promise.reject('my test promise - unhandledRejection');
 
   const config = new DocumentBuilder()
     .setTitle('POSTGRESQL AND LAYERED ARCHITECTURE')
--- src/middlewares/logger.ts
+++ src/middlewares/logger.ts
@@ -3,42 +3,43 @@ import winston from 'winston';
 import * as util from 'util';
 import 'dotenv/config';
 
+export const winstonLogger = winston.createLogger({
+  format: winston.format.combine(
+    winston.format.colorize(),
+    winston.format.label({
+      label: '[winstonLogger]',
+    }),
+    winston.format.timestamp({
+      format: 'YY-MM-DD HH:mm:ss',
+    }),
+    winston.format.printf((info) => {
+      let format = `${info.label}  level: ${info.level}  message: ${info.message}  ${info.timestamp}  path: ${info.path}  method: ${info.method}`;
+      if (process.env.LOG_REQUEST_RESPONSE !== 'false') {
+        format = format.concat(
+          `request: ${util.inspect(info.request)}\nresponse: ${util.inspect(
+            info.response,
+          )}`,
+        );
+      }
+      return format;
+    }),
+  ),
+  transports: [
+    new winston.transports.Console({
+      level: 'error',
+    }),
+    new winston.transports.Console({
+      level: 'info',
+    }),
+  ],
+});
+
 export function myLogger(
   request: Request,
   response: Response,
   next: NextFunction,
 ) {
-  const logger = winston.createLogger({
-    format: winston.format.combine(
-      winston.format.colorize(),
-      winston.format.label({
-        label: '[winstonLogger]',
-      }),
-      winston.format.timestamp({
-        format: 'YY-MM-DD HH:mm:ss',
-      }),
-      winston.format.printf((info) => {
-        let format = `${info.label}  level: ${info.level}  ${info.timestamp}  path: ${info.path}  method: ${info.method}`;
-        if (process.env.LOG_REQUEST_RESPONSE !== 'false') {
-          format = format.concat(
-            `request: ${util.inspect(info.request)}\nresponse: ${util.inspect(
-              info.response,
-            )}`,
-          );
-        }
-        return format;
-      }),
-    ),
-    transports: [
-      new winston.transports.Console({
-        level: 'error',
-      }),
-      new winston.transports.Console({
-        level: 'info',
-      }),
-    ],
-  });
-  logger.log({
+  winstonLogger.log({
     level: 'info',
     message: 'info',
     path: request.path,
