9b5808c9bf34234b07f47f852e4b40252311c3d5
anna-left <anna_left@mail.ru>
2022-08-21 19:42:47 +0300

    feat: use DB port from .env

--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -22,6 +22,7 @@ import { AuthMiddleware } from 'src/middlewares/auth.middleware';
     SequelizeModule.forRoot({
       dialect: 'postgres',
       host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_DATABASE,
